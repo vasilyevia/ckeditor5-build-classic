4f41a7a018ef6d82c0f333a54a2f4da65028e2b5
Ivan Vasilyev <vasilyevia@gmail.com>
2019-08-05 16:38:18 +0700

    custom build

--- src/Plugins/Variables/Variables.js
+++ src/Plugins/Variables/Variables.js
@@ -1,51 +1,51 @@
 /* global console */
 
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import {
 	createDropdown,
 	addListToDropdown
-} from "@ckeditor/ckeditor5-ui/src/dropdown/utils";
-import Collection from "@ckeditor/ckeditor5-utils/src/collection";
-import Model from "@ckeditor/ckeditor5-ui/src/model";
+} from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
 
 export default class Variables extends Plugin {
 	init() {
 		const editor = this.editor;
 
-		editor.ui.componentFactory.add("variables", locale => {
-			const dropdown = createDropdown(locale);
+		editor.ui.componentFactory.add( 'variables', locale => {
+			const dropdown = createDropdown( locale );
 
-			dropdown.buttonView.set({
-				label: "Available variables",
+			dropdown.buttonView.set( {
+				label: 'Available variables',
 				withText: true
-			});
+			} );
 
 			const items = new Collection();
 
-			items.add({
-				type: "button",
-				model: new Model({
+			items.add( {
+				type: 'button',
+				model: new Model( {
 					withText: true,
-					label: "First item",
-					labelStyle: "color: red"
-				})
-			});
+					label: 'First item',
+					labelStyle: 'color: red'
+				} )
+			} );
 
-			items.add({
-				type: "button",
-				model: new Model({
+			items.add( {
+				type: 'button',
+				model: new Model( {
 					withText: true,
-					label: "Second item",
-					labelStyle: "color: green",
-					class: "foo"
-				})
-			});
+					label: 'Second item',
+					labelStyle: 'color: green',
+					class: 'foo'
+				} )
+			} );
 
-			addListToDropdown(dropdown, items);
+			addListToDropdown( dropdown, items );
 
 			return dropdown;
-		});
+		} );
 
-		console.log("Variables plugin was initialized");
+		console.log( 'Variables plugin was initialized' );
 	}
 }

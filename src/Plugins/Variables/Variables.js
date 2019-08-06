c450efba5fc432c8fa08b2502d08a6608764aad0
Ivan Vasilyev <vasilyevia@gmail.com>
2019-08-06 19:42:21 +0700

    custom build

--- src/Plugins/Variables/Variables.js
+++ src/Plugins/Variables/Variables.js
@@ -1,61 +1,61 @@
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-import Collection from "@ckeditor/ckeditor5-utils/src/collection";
-import Model from "@ckeditor/ckeditor5-ui/src/model";
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
 import {
 	createDropdown,
 	addListToDropdown
-} from "@ckeditor/ckeditor5-ui/src/dropdown/utils";
+} from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
 
 export default class Variables extends Plugin {
 	init() {
 		const editor = this.editor;
 
-		var variables = editor.config.get("variables");
+		const variables = editor.config.get( 'variables' );
 
-		editor.ui.componentFactory.add("variables", locale => {
-			const dropdown = createDropdown(locale);
+		editor.ui.componentFactory.add( 'variables', locale => {
+			const dropdown = createDropdown( locale );
 
-			dropdown.buttonView.set({
-				label: "Variables",
+			dropdown.buttonView.set( {
+				label: 'Variables',
 				withText: true
-			});
+			} );
 
-			if (!Array.isArray(variables) || variables.length < 1) {
-				//hide dropdown
+			if ( !Array.isArray( variables ) || variables.length < 1 ) {
+				// hide dropdown
 
 				dropdown.buttonView.isVisible = false;
 			} else {
-				//populate dropdown
+				// populate dropdown
 
 				const items = new Collection();
 
-				for (var i in variables) {
-					const variable = variables[i];
+				for ( const i in variables ) {
+					const variable = variables[ i ];
 
-					items.add({
-						type: "button",
-						model: new Model({
+					items.add( {
+						type: 'button',
+						model: new Model( {
 							withText: true,
 							label: variable.text,
 							code: variable.code
-						})
-					});
+						} )
+					} );
 				}
 
-				addListToDropdown(dropdown, items);
+				addListToDropdown( dropdown, items );
 
-				dropdown.on("execute", evtInfo => {
+				dropdown.on( 'execute', evtInfo => {
 					const button = evtInfo.source;
 					const code = button.code;
 					const cursorPosition = editor.model.document.selection.getFirstPosition();
 
-					editor.model.change(writer => {
-						writer.insertText(code, cursorPosition);
-					});
-				});
+					editor.model.change( writer => {
+						writer.insertText( code, cursorPosition );
+					} );
+				} );
 			}
 
 			return dropdown;
-		});
+		} );
 	}
 }
